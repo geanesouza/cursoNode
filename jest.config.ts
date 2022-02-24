3911768cf04f4d8811d13cbca6aa45035fb4ef39
Geane Araújo <araujoge@creathus.org.br>
2022-02-24 18:30:15 -0400

    chore: add jest to lint staged

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverage: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts:jest'
+    '.+\\.ts$': 'ts-jest'
   }
 }
